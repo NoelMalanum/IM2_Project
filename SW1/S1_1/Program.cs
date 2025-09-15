ff2738915151c3ee9bfcd27658e61aa35cb096e4
NOEL <malanumnoel9@gmail.com>
2025-09-15 22:06:50 +0800

    upload

---
+++ SW1/S1_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1_1
+{
+    class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main()
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            int count = 1;
+            foreach (string s in numbers)
+            {
+                if (s == "") continue;
+                int num = int.Parse(s);
+                Console.WriteLine($"{count}. {num}\t" + (IsPrime(num) ? "Prime" : "Composite"));
+                count++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SW1/S1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1_2
+{
+    class Program
+    {
+        static (double peso, double yen) ConvertCurrency(double dollars)
+        {
+            double peso = dollars * 57.17;
+            double yen = dollars * 146.67;
+            return (peso, yen); 
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter currency in ($): ");
+            double dollars = double.Parse(Console.ReadLine());
+
+            var result = ConvertCurrency(dollars);
+
+            Console.WriteLine("\nDollar($)\tPhil Peso(P)\tJpn Yen(Y)");
+            Console.WriteLine($"{dollars}\t\t{result.peso}\t\t{result.yen}");
+
+            Console.ReadKey();
+        }
+    }
+}
